8274896ea518273eb328b47d07d717122396221a
Masahiro Nakagawa <repeatedly@gmail.com>
2014-02-28 13:08:32 +0000

    Add installing dependent gem mechanizm

---
+++ core_gems.rb
@@ -0,0 +1,8 @@
+dir 'score_gems'
+download "bundler", "1.2.5"
+download "json", "1.7.7"
+download "msgpack", "0.4.7"
+download "iobuffer", "1.1.2"
+download "cool.io", "1.1.1"
+download "http_parser.rb", "0.5.1"
+download "yajl-ruby", "1.1.0"
---
+++ lib/gems_parser.rb
@@ -0,0 +1,26 @@
+class GemsParser
+  def self.parse(file)
+    gp = GemsParser.new
+    gp.parse(file)
+    gp
+  end
+
+  def initialize
+    @target_dir = nil
+    @target_files = []
+  end
+
+  attr_reader :target_dir, :target_files
+
+  def parse(file)
+    self.instance_eval(file)
+  end
+
+  def dir(path)
+    @target_dir = path
+  end
+
+  def download(name, ver)
+    @target_files << [name, ver]
+  end
+end
