36261e867c8339ef6b58f9247b263f82b7c65f33
Masahiro Nakagawa <repeatedly@gmail.com>
2014-02-28 13:09:14 +0000

    Separate td-agent.rb into fluentd and td-agent softwares

---
+++ config/software/fluentd.rb
@@ -0,0 +1,22 @@
+name "fluentd"
+version '098fea17aedab0ebf58aa55ace76d14ad0889262'
+#version '1229278fe48ddc449ad501aa9c114b4597a794da' # git ref
+
+dependency "ruby"
+dependency "bundler"
+
+source :git => 'https://github.com/fluent/fluentd.git'
+relative_path "fluentd"
+
+env = {
+  #"PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
+}
+
+build do
+  Dir.glob(File.expand_path(File.join(project_root, 'core_gems', '*.gem'))).sort.each { |gem_path|
+    command "sudo #{install_dir}/embedded/bin/gem install --no-ri --no-rdoc #{gem_path}", :env => env
+  }
+  command "rake build", :env => env
+  command "sudo #{install_dir}/embedded/bin/gem install --no-ri --no-rdoc pkg/fluentd-*.gem", :env => env
+  command "sudo ln -fs #{install_dir}/embedded/bin/fluentd #{install_dir}/bin/fluentd", :env => env
+end
--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -1,23 +1,15 @@
 name "td-agent"
-version '1229278fe48ddc449ad501aa9c114b4597a794da' # git ref
-gem_version = '0.10.44'  # local_variable
+#version '' # git ref
 
 dependency "ruby"
-#dependency "rubygems"
 dependency "bundler"
+dependency "fluentd"
+dependency "nokogiri"
 
-#source :git => "https://github.com/fluent/fluentd.git"
-source :git => 'https://github.com/fluent/fluentd.git'
-relative_path "fluentd"
-
-env = {
-  #"PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
-}
+env = {}
 
 build do
-  #command "sudo #{install_dir}/embedded/bin/gem install json", :env => env
-  command "rake build", :env => env
-  #rake "gem", :env => env
-  command "sudo #{install_dir}/embedded/bin/gem install --no-ri --no-rdoc pkg/fluentd-*.gem", :env => env
-  command "sudo ln -fs #{install_dir}/embedded/bin/fluentd #{install_dir}/bin/fluentd", :env => env
+  Dir.glob(File.expand_path(File.join(project_root, 'plugin_gems', '*.gem'))).sort.each { |gem_path|
+    command "sudo #{install_dir}/embedded/bin/gem install --no-ri --no-rdoc #{gem_path}", :env => env
+  }
 end
