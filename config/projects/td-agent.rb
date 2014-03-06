18469cae663c14fd6b0f71cf8791dae7363915d5
Masahiro Nakagawa <repeatedly@gmail.com>
2014-03-06 00:37:14 +0000

    extra files, e.g. init script, configuration files

--- config/projects/td-agent.rb
+++ config/projects/td-agent.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 
 name "td-agent"
 maintainer "Treasure Data, Inc"
@@ -16,6 +17,23 @@ dependency "td-agent"
 
 # version manifest file
 dependency "version-manifest"
+
+# copy pre/post scripts into omnibus path
+pkg_type = package_types.first
+Dir.glob(File.join(package_scripts_path, pkg_type, '*')).each { |f|
+  FileUtils.copy(f, package_scripts_path)
+}
+
+# copy init.d file
+initd_path = File.join(files_path, 'etc', 'init.d')
+FileUtils.copy(File.join(initd_path, pkg_type, 'td-agent'), initd_path)
+
+# fpm doesn't support missingok yet?
+#config_file "#{install_path}/etc/td-agent/td-agent.conf"
+extra_package_file "#{install_path}/etc/td-agent/td-agent.conf.tmpl"
+extra_package_file "#{install_path}/etc/td-agent/prelink.conf.d/td-agent.prelink.conf"
+extra_package_file "#{install_path}/etc/td-agent/logrotate.d/td-agent.logrotate"
+extra_package_file "#{install_path}/etc/init.d/td-agent"
 
 exclude "\.git*"
 exclude "bundler\/git"
--- config/software/td-agent.rb
+++ config/software/td-agent.rb
@@ -3,6 +3,7 @@ name "td-agent"
 
 dependency "ruby"
 dependency "bundler"
+dependency "jemalloc"
 dependency "fluentd"
 dependency "nokogiri"
 
@@ -12,4 +13,6 @@ build do
   Dir.glob(File.expand_path(File.join(project_root, 'plugin_gems', '*.gem'))).sort.each { |gem_path|
     command "sudo #{install_dir}/embedded/bin/gem install --no-ri --no-rdoc #{gem_path}", :env => env
   }
+
+  command "rsync -a #{Omnibus.project_root}/files/ #{install_dir}/"
 end
