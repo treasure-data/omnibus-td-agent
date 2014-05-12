eb19c9e6e3c20e2f4660b1722e244750798d65cb
Masahiro Nakagawa <repeatedly@gmail.com>
2014-05-12 04:49:31 -0700

    Update cool.io version

--- core_gems.rb
+++ core_gems.rb
@@ -2,6 +2,6 @@ dir 'core_gems'
 download "bundler", "1.6.1"
 download "json", "1.8.1"
 download "msgpack", "0.5.8"
-download "cool.io", "1.2.1"
+download "cool.io", "1.2.4"
 download "http_parser.rb", "0.6.0"
 download "yajl-ruby", "1.2.0"
