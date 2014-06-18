13e6f31f382e8856fcae540d53a7a6c4db5ac6d1
Masahiro Nakagawa <repeatedly@gmail.com>
2014-06-19 06:17:59 +0900

    Update core gems

--- core_gems.rb
+++ core_gems.rb
@@ -1,8 +1,8 @@
 dir 'core_gems'
-download "bundler", "1.6.1"
+download "bundler", "1.6.3"
 download "json", "1.8.1"
 download "msgpack", "0.5.8"
 download "cool.io", "1.2.4"
 download "http_parser.rb", "0.6.0"
 download "yajl-ruby", "1.2.1"
-download "rspec", "2.14.1"
+download "rspec", "3.0.0"
