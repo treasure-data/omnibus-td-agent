ede379114990e7c0b06cc4b03502c0c5148afd00
Masahiro Nakagawa <repeatedly@gmail.com>
2015-02-06 09:59:14 +0900

    Update bundled gems

--- core_gems.rb
+++ core_gems.rb
@@ -1,11 +1,11 @@
 dir 'core_gems'
 download "bundler", "1.7.4"
 download "json", "1.8.1"
-download "msgpack", "0.5.8"
+download "msgpack", "0.5.11"
 download "cool.io", "1.2.4"
 download "http_parser.rb", "0.6.0"
 download "yajl-ruby", "1.2.1"
 download "sigdump", "0.2.2"
 download "thread_safe", "0.3.4"
 download "tzinfo", "1.2.2"
-download "tzinfo-data", "1.2014.10"
+download "tzinfo-data", "1.2015.1"
