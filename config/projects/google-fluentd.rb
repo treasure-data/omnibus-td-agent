157e6be3f101a31e748d5d1b1fc6d01ffd9912a6
Todd Derr <salty@google.com>
2015-03-13 17:04:13 -0400

    Rename td-agent2.rb to google-fluentd.rb
    This should have been done initially, so we don't miss upstream changes.
    
    Note this leaves things in an inconsistent state since the google-fluentd
    changes are missing; another commit will follow.

--- config/projects/google-fluentd.rb
+++ config/projects/google-fluentd.rb
@@ -2,14 +2,14 @@ require 'erb'
 require 'fileutils'
 require 'rubygems'
 
-name "google-fluentd"
-maintainer "Google, Inc."
-homepage "http://cloud.google.com/logging/docs/"
-description "Google Fluentd: A data collector for Google Cloud Logging"
+name "td-agent"
+maintainer "Treasure Data, Inc"
+homepage "http://treasuredata.com"
+description "Treasure Agent: A data collector for Treasure Data"
 
-install_dir     "/opt/google-fluentd"
-build_version   "1.3.0"
-build_iteration 1
+install_dir     "/opt/td-agent"
+build_version   "2.1.4"
+build_iteration 0
 
 # creates required build directories
 dependency "preparation"
