0c731d4d700731bc9f269bfbf90bca1d922699b7
Todd Derr <salty@google.com>
2015-03-13 16:54:47 -0400

    Add a Google Cloud-specific default config file.
    Bump the version to 1.3.0 due to to the config file change.

--- config/projects/google-fluentd.rb
+++ config/projects/google-fluentd.rb
@@ -8,8 +8,8 @@ homepage "http://cloud.google.com/logging/docs/"
 description "Google Fluentd: A data collector for Google Cloud Logging"
 
 install_dir     "/opt/google-fluentd"
-build_version   "1.2.2"
-build_iteration 0
+build_version   "1.3.0"
+build_iteration 1
 
 # creates required build directories
 dependency "preparation"
