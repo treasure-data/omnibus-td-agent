03f2b58fcf01e65ab3bd8c3ae2fc66a87679e948
Masahiro Nakagawa <repeatedly@gmail.com>
2015-03-27 11:00:01 +0900

    v2.1.5-1

--- config/projects/td-agent2.rb
+++ config/projects/td-agent2.rb
@@ -9,7 +9,7 @@ description "Treasure Agent: A data collector for Treasure Data"
 
 install_dir     "/opt/td-agent"
 build_version   "2.1.5"
-build_iteration 0
+build_iteration 1
 
 # creates required build directories
 dependency "preparation"
