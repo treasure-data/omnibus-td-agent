3a9ea19211ee5213aab400f3869ec7be604b9d15
Todd Derr <salty@google.com>
2015-09-15 15:13:26 -0400

    Get rid of obsolete ncurses workaround.
    Since we pull omnibus from github and the ncurses change has been merged,
    we don't need the workaround anymore.

--- config/projects/td-agent2.rb
+++ config/projects/td-agent2.rb
@@ -22,8 +22,6 @@ override :postgresql, :version => '9.3.5'
 if ohai['platform_family'] == 'rhel' && ohai['platform_version'].split('.').first.to_i == 7
   override :liblzma, :version => '5.1.2alpha'
 end
-# workaround until https://github.com/chef/omnibus-software/pull/473 is live.
-override :ncurses, :version => '5.9'
 
 # td-agent dependencies/components
 dependency "td-agent"
