1a8c2335dbe1a96467b5307eaedf05ce68994adb
sam <Sam Pointer>
2017-01-31 23:51:33 +0000

    add documentation about custom plugin builds

--- config/projects/td-agent2.rb
+++ config/projects/td-agent2.rb
@@ -8,8 +8,8 @@ homepage "http://treasuredata.com"
 description "Treasure Agent: A data collector for Treasure Data"
 
 install_dir     "/opt/td-agent"
-build_version   "2.3.1"
-build_iteration 3
+build_version   "0.0.0"
+build_iteration 6
 
 # creates required build directories
 dependency "preparation"
