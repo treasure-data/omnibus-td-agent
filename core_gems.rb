f1d3212edfa56ec6fae0b00cfad49e10e50ec7a2
Masahiro Nakagawa <repeatedly@gmail.com>
2017-03-02 07:38:50 +0900

    Fix gem version of win32-api

--- core_gems.rb
+++ core_gems.rb
@@ -18,7 +18,6 @@ if windows?
   download 'win32-event', '0.6.3'
   download 'win32-service', '0.8.10'
   download 'windows-pr', '1.2.6'
-  download 'win32-api', '1.6.1.2'
+  download 'win32-api', '1.6.1.2-universal-mingw32'
   download 'windows-api', '0.4.4'
 end
-
