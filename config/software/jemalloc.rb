baa2a0ae32a0123ab9ea527538b8d78e99620723
Dhrupad Bhardwaj <dhrupad@google.com>
2017-08-29 12:08:13 -0400

    Change repository for jemalloc from canonware.com to pkgs.fedoraproject.org (#47)

--- config/software/jemalloc.rb
+++ config/software/jemalloc.rb
@@ -2,14 +2,25 @@ name "jemalloc"
 default_version "4.2.1"
 
 # for td-agent
-version("2.2.5") { source :md5 => 'a5c4332705ed0e3fff1ac73cfe975640' }
-version("3.6.0") { source :md5 => 'e76665b63a8fddf4c9f26d2fa67afdf2' }
-version("4.2.1") { source :md5 => '094b0a7b8c77c464d0dc8f0643fd3901' }
+version("2.2.5") do
+  md5 = 'a5c4332705ed0e3fff1ac73cfe975640'
+  source :md5 => md5,
+         :url => "http://pkgs.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{md5}/jemalloc-#{version}.tar.bz2"
+end
+version("3.6.0") do
+  md5 = 'e76665b63a8fddf4c9f26d2fa67afdf2'
+  source :md5 => md5,
+         :url => "http://pkgs.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{md5}/jemalloc-#{version}.tar.bz2"
+end
+version("4.2.1") do
+  md5 = '094b0a7b8c77c464d0dc8f0643fd3901'
+  source :md5 => md5,
+         :url => "http://pkgs.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{md5}/jemalloc-#{version}.tar.bz2"
+end
 
 # On Mac, this file blocks package building at health check so add to whitelist
 whitelist_file "libjemalloc\.1\.dylib"
 
-source :url => "http://www.canonware.com/download/jemalloc/jemalloc-#{version}.tar.bz2"
 relative_path "jemalloc-#{version}"
 
 env = with_standard_compiler_flags(with_embedded_path)
