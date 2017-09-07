4a1f5b7fb4dcc446f942bb88f30a4051f368d04b
Masahiro Nakagawa <repeatedly@gmail.com>
2017-09-07 19:27:56 +0900

    Add rubygems software for 2.6.13 to fix security issues. This is temporarl solution

--- config/projects/td-agent2.rb
+++ config/projects/td-agent2.rb
@@ -18,7 +18,7 @@ dependency "preparation"
 
 override :ruby, :version => '2.1.10' # This override version is used for gem_dir_version. See td-agent-files.rb
 override :zlib, :version => '1.2.8'
-override :rubygems, :version => '2.4.8'
+override :rubygems, :version => '2.6.13'
 override :postgresql, :version => '9.3.5'
 override :fluentd, :version => 'd5e0a61e06a7cfeb7266b018e77ac74f85c0c06d' # v0.12.40
 
