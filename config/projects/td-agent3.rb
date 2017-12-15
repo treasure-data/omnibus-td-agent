1f38bd4f9efc699b2db7655cac737ff88adf0fe3
Masahiro Nakagawa <repeatedly@gmail.com>
2017-12-15 17:49:25 +0900

    Update fluentd and jemalloc version

--- config/projects/td-agent3.rb
+++ config/projects/td-agent3.rb
@@ -16,10 +16,10 @@ dependency "preparation"
 
 override :ruby, :version => '2.4.2'
 override :zlib, :version => '1.2.8'
-override :jemalloc, :version => '5.0.1'
+override :jemalloc, :version => '4.5.0'
 override :rubygems, :version => '2.6.7'
 override :postgresql, :version => '9.5.5'
-override :fluentd, :version => '363b1af858aab34d90d9a98d63107b1206afd141' # v0.14.25
+override :fluentd, :version => '4cfd824d4269437cc929c0a42e67a53aa5d16056' # v1.0.1
 
 # td-agent dependencies/components
 dependency "td-agent"
