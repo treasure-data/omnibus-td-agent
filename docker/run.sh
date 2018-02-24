24a4cda1ff44f5581dfb02c420877872bec25c20
bmoyles0117 <bmoy117@gmail.com>
2018-02-23 19:20:34 -0500

    Switch to stretch based image and include systemd by default. (#80)
    Also disable fluentd-catch-all-config.

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,6 @@
 # For systems without journald.
 mkdir -p /var/log/journal
 
-# Copy host libsystemd into image to avoid compatibility issues. This operation
-# is only performed if the host's lib is mounted into the container through a 
-# volume such as -v /usr/lib64/:/host/lib/.
-if [ -n "$(ls /host/lib/libsystemd* 2>/dev/null)" ]; then
-  rm /lib/x86_64-linux-gnu/libsystemd*
-  ln -s /host/lib/libsystemd* /lib/x86_64-linux-gnu/
-fi
-
 if [ -z "${METADATA_AGENT_URL:-}" -a -n "${METADATA_AGENT_HOSTNAME:-}" ]; then
   METADATA_AGENT_URL="http://${METADATA_AGENT_HOSTNAME}:8000"
 fi
