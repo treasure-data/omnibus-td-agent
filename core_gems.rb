9522622e3d6d079ae5dc36eec2fdcb35325582df
Masahiro Nakagawa <repeatedly@gmail.com>
2018-04-11 08:56:56 +0900

    Update core gems

--- core_gems.rb
+++ core_gems.rb
@@ -1,5 +1,5 @@
 dir 'core_gems'
-download "bundler", "1.16.0"
+download "bundler", "1.16.1"
 download "msgpack", "1.2.4"
 if td_agent_2?
   download "cool.io", "1.5.1"
@@ -18,15 +18,15 @@ download "tzinfo", "1.2.5"
 download "tzinfo-data", "1.2018.4"
 unless td_agent_2?
   download "dig_rb", "1.0.1"
-  download 'serverengine', '2.0.5'
+  download 'serverengine', '2.0.6'
 end
 if windows?
-  download 'ffi', '1.9.18'
+  download 'ffi', '1.9.23'
   download 'ffi-win32-extensions', '1.0.3'
   download 'win32-ipc', '0.7.0'
   download 'win32-event', '0.6.3'
   download 'win32-service', '0.8.10'
   download 'windows-pr', '1.2.6'
-  download 'win32-api', '1.7.0-universal-mingw32'
+  download 'win32-api', '1.7.1-universal-mingw32'
   download 'windows-api', '0.4.4'
 end
