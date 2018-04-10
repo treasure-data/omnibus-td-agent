9be08daccc5dab6a4607902119e2344f987c1e04
Masahiro Nakagawa <repeatedly@gmail.com>
2018-04-11 08:57:14 +0900

    Update plugin gems

--- plugin_gems.rb
+++ plugin_gems.rb
@@ -5,22 +5,22 @@ if td_agent_2?
   download "td", "0.15.2"
   download "fluent-plugin-td", "0.10.29"
 else
-  download "td-client", "1.0.4"
-  download "td", "0.15.7"
+  download "td-client", "1.0.6"
+  download "td", "0.15.9"
   download "fluent-plugin-td", "1.0.0"
 end
 if td_agent_2?
   download "aws-sdk", "2.11.22"
   download "fluent-plugin-s3", "0.8.7"
 else
-  download "jmespath", "1.3.1"
-  download "aws-partitions", "1.42.0"
+  download "jmespath", "1.4.0"
+  download "aws-partitions", "1.79.0"
   download "aws-sigv4", "1.0.2"
-  download "aws-sdk-core", "3.11.0"
-  download "aws-sdk-kms", "1.3.0"
+  download "aws-sdk-core", "3.19.0"
+  download "aws-sdk-kms", "1.5.0"
   download "aws-sdk-sqs", "1.3.0"
-  download "aws-sdk-s3", "1.8.0"
-  download "fluent-plugin-s3", "1.1.0"
+  download "aws-sdk-s3", "1.9.0"
+  download "fluent-plugin-s3", "1.1.2"
 end
 if td_agent_2?
   download "thrift", "0.8.0"
@@ -33,18 +33,18 @@ download "webhdfs", "0.8.0"
 if td_agent_2?
   download "fluent-plugin-webhdfs", "0.7.1"
 else
-  download "fluent-plugin-webhdfs", "1.2.2"
+  download "fluent-plugin-webhdfs", "1.2.3"
 end
 if td_agent_2?
   download "fluent-plugin-rewrite-tag-filter", "1.6.0"
 else
-  download "fluent-plugin-rewrite-tag-filter", "2.0.1"
+  download "fluent-plugin-rewrite-tag-filter", "2.0.2"
 end
 download "ruby-kafka", "0.5.4"
 download "fluent-plugin-kafka", "0.7.2"
 unless td_agent_2?
-  download "elasticsearch", "5.0.4"
-  download "fluent-plugin-elasticsearch", "2.4.0"
+  download "elasticsearch", "6.0.2"
+  download "fluent-plugin-elasticsearch", "2.8.6"
 end
 download "fluent-plugin-td-monitoring", "0.2.4"
 if windows?
