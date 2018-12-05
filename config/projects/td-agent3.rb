6a3b052269032ab026d0f1387dfc5d59199eae04
Masahiro Nakagawa <repeatedly@gmail.com>
2018-12-06 04:30:21 +0900

    update fluentd to v1.3.1

--- config/projects/td-agent3.rb
+++ config/projects/td-agent3.rb
@@ -24,7 +24,7 @@ override :zlib, :version => '1.2.11'
 override :jemalloc, :version => '4.5.0'
 override :rubygems, :version => '2.6.14'
 override :postgresql, :version => '9.6.9'
-override :fluentd, :version => 'f30865f82a0d19730940a52c7cb12de88d1821fb' # v1.2.6
+override :fluentd, :version => '3dabdc5c42918049933156db7c07437346b76773' # v1.3.1
 
 # td-agent dependencies/components
 dependency "td-agent"
