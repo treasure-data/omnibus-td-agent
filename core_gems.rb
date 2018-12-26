3475b8b3ea7654090c1ff5b664ad82a2c65cdf35
Masahiro Nakagawa <repeatedly@gmail.com>
2018-12-26 18:45:37 +0900

    Update core gems

--- core_gems.rb
+++ core_gems.rb
@@ -12,21 +12,21 @@ download "sigdump", "0.2.4"
 if td_agent_2?
   download "oj", "2.18.5"
 else
-  download "oj", "3.3.9"
+  download "oj", "3.7.4"
 end
 download "tzinfo", "1.2.5"
-download "tzinfo-data", "1.2018.5"
+download "tzinfo-data", "1.2018.7"
 unless td_agent_2?
   download "dig_rb", "1.0.1"
-  download 'serverengine', '2.0.7'
+  download 'serverengine', '2.1.0'
 end
 if windows?
   download 'ffi', '1.9.25'
   download 'ffi-win32-extensions', '1.0.3'
   download 'win32-ipc', '0.7.0'
   download 'win32-event', '0.6.3'
-  download 'win32-service', '0.8.10'
-  download 'windows-pr', '1.2.6'
+  download 'win32-service', '1.0.1'
   download 'win32-api', '1.7.1-universal-mingw32'
+  download 'windows-pr', '1.2.6'
   download 'windows-api', '0.4.4'
 end
