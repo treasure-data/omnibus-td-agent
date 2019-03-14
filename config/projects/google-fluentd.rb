4d00385c405867ab428a3d4eeb2bd4cdb6a60a72
Ling Huang <qingling128@users.noreply.github.com>
2019-03-14 18:51:24 -0400

    Bump versions for future releases. (#160)

--- config/projects/google-fluentd.rb
+++ config/projects/google-fluentd.rb
@@ -8,7 +8,7 @@ homepage "http://cloud.google.com/logging/docs/"
 description "Google Fluentd: A data collector for Google Cloud Logging"
 
 install_dir     "/opt/google-fluentd"
-build_version   "1.6.4"
+build_version   "1.6.5"
 build_iteration 1
 
 # creates required build directories
