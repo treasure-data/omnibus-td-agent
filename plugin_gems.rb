8a1b78161d01e7e9587d2e3e530ab1f35a982230
Masahiro Nakagawa <repeatedly@gmail.com>
2019-04-15 07:50:09 +0900

    Update plugin gems

--- plugin_gems.rb
+++ plugin_gems.rb
@@ -14,13 +14,13 @@ if td_agent_2?
   download "fluent-plugin-s3", "0.8.7"
 else
   download "jmespath", "1.4.0"
-  download "aws-partitions", "1.127.0"
-  download "aws-sigv4", "1.0.3"
-  download "aws-sdk-core", "3.44.2"
-  download "aws-sdk-kms", "1.13.0"
-  download "aws-sdk-sqs", "1.10.0"
-  download "aws-sdk-s3", "1.30.0"
-  download "fluent-plugin-s3", "1.1.7"
+  download "aws-partitions", "1.149.0"
+  download "aws-sigv4", "1.1.0"
+  download "aws-sdk-core", "3.48.3"
+  download "aws-sdk-kms", "1.16.0"
+  download "aws-sdk-sqs", "1.13.0"
+  download "aws-sdk-s3", "1.36.0"
+  download "fluent-plugin-s3", "1.1.9"
 end
 if td_agent_2?
   download "thrift", "0.8.0"
@@ -40,18 +40,19 @@ if td_agent_2?
 else
   download "fluent-plugin-rewrite-tag-filter", "2.1.1"
 end
-download "ruby-kafka", "0.7.4"
-download "fluent-plugin-kafka", "0.8.3"
+download "ruby-kafka", "0.7.6"
+download "rdkafka", "0.4.2"
+download "fluent-plugin-kafka", "0.9.2"
 unless td_agent_2?
-  download "elasticsearch", "6.1.0"
-  download "fluent-plugin-elasticsearch", "3.0.1"
+  download "elasticsearch", "6.3.0"
+  download "fluent-plugin-elasticsearch", "3.4.2"
   download "prometheus-client", "0.9.0"
   download "fluent-plugin-prometheus", "1.3.0"
 end
 if td_agent_2?
   download "fluent-plugin-record-modifier", "0.6.2"
 else
-  download "fluent-plugin-record-modifier", "1.1.0"
+  download "fluent-plugin-record-modifier", "2.0.1"
 end
 download "fluent-plugin-td-monitoring", "0.2.4"
 if windows?
