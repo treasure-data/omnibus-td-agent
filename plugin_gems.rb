54bafc1f63dec64a8279877ca5b2441fb6b6b05a
stackdriver-instrumentation-release <49999651+stackdriver-instrumentation-release@users.noreply.github.com>
2019-05-09 13:56:22 -0400

    Automated: Update REPO_SUFFIX to 20190505-1 and bump versions for future releases. (#169)

--- plugin_gems.rb
+++ plugin_gems.rb
@@ -22,7 +22,7 @@ download "fluent-plugin-rewrite-tag-filter", "1.5.5"
 # This is an indirect dependency of fluent-plugin-td-monitoring.
 download "mixlib-cli", "1.7.0"
 download "fluent-plugin-td-monitoring", "0.2.2"
-download "fluent-plugin-google-cloud", "0.7.10"
+download "fluent-plugin-google-cloud", "0.7.11"
 download "fluent-plugin-detect-exceptions", "0.0.10"
 download "prometheus-client", "0.4.2"
 download "fluent-plugin-multi-format-parser", "0.1.1"
