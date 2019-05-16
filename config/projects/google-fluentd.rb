352965c177630fca75e973d22486834831d4db44
igorpeshansky <igorpeshansky@users.noreply.github.com>
2019-05-16 17:19:36 -0400

    Update default embedded Ruby version from 2.4 to 2.5. (#172)

--- config/projects/google-fluentd.rb
+++ config/projects/google-fluentd.rb
@@ -14,7 +14,7 @@ build_iteration 1
 # creates required build directories
 dependency "preparation"
 
-override :ruby, :version => '2.4.3'
+override :ruby, :version => '2.5.3'
 override :zlib, :version => '1.2.8'
 override :rubygems, :version => '2.4.8'
 override :postgresql, :version => '9.3.5'
