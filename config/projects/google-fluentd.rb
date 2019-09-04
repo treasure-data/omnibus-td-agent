cf92afa7216e54e4e6bfe868b63f1c427f98bf3f
stackdriver-instrumentation-release <49999651+stackdriver-instrumentation-release@users.noreply.github.com>
2019-09-04 18:49:54 -0400

    Automated: Update REPO_SUFFIX to 20190829-1 and bump versions for future releases. (#200)

--- config/projects/google-fluentd.rb
+++ config/projects/google-fluentd.rb
@@ -8,7 +8,7 @@ homepage "http://cloud.google.com/logging/docs/"
 description "Google Fluentd: A data collector for Google Cloud Logging"
 
 install_dir     "/opt/google-fluentd"
-build_version   "1.6.17"
+build_version   "1.6.18"
 build_iteration 1
 
 # creates required build directories
