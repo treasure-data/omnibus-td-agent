3580dbabf7fa0555085e1eea617f05c810c83c90
Masahiro Nakagawa <repeatedly@gmail.com>
2020-01-24 16:40:39 +0900

    Update plugin gems

--- plugin_gems.rb
+++ plugin_gems.rb
@@ -6,7 +6,7 @@ if td_agent_2?
   download "fluent-plugin-td", "0.10.29"
 else
   download "td-client", "1.0.7"
-  download "td", "0.16.7"
+  download "td", "0.16.8"
   download "fluent-plugin-td", "1.1.0"
 end
 if td_agent_2?
@@ -14,13 +14,13 @@ if td_agent_2?
   download "fluent-plugin-s3", "0.8.7"
 else
   download "jmespath", "1.4.0"
-  download "aws-partitions", "1.230.0"
+  download "aws-partitions", "1.268.0"
   download "aws-sigv4", "1.1.0"
-  download "aws-sdk-core", "3.72.0"
-  download "aws-sdk-kms", "1.25.0"
-  download "aws-sdk-sqs", "1.23.0"
-  download "aws-sdk-s3", "1.52.0"
-  download "fluent-plugin-s3", "1.2.0"
+  download "aws-sdk-core", "3.89.1"
+  download "aws-sdk-kms", "1.28.0"
+  download "aws-sdk-sqs", "1.23.1"
+  download "aws-sdk-s3", "1.60.1"
+  download "fluent-plugin-s3", "1.2.1"
 end
 if td_agent_2?
   download "thrift", "0.8.0"
@@ -29,7 +29,7 @@ if td_agent_2?
   download "mongo", "2.2.7"
   download "fluent-plugin-mongo", "0.8.1"
 end
-download "webhdfs", "0.8.0"
+download "webhdfs", "0.9.0"
 if td_agent_2?
   download "fluent-plugin-webhdfs", "0.7.1"
 else
@@ -44,17 +44,17 @@ download "ruby-kafka", "0.7.10"
 unless windows?
   download "rdkafka", "0.7.0"
 end
-download "fluent-plugin-kafka", "0.12.1"
+download "fluent-plugin-kafka", "0.12.2"
 unless td_agent_2?
-  download "elasticsearch", "6.8.0"
-  download "fluent-plugin-elasticsearch", "3.5.5"
+  download "elasticsearch", "6.8.1"
+  download "fluent-plugin-elasticsearch", "3.8.0"
   download "prometheus-client", "0.9.0"
   download "fluent-plugin-prometheus", "1.7.0"
 end
 if td_agent_2?
   download "fluent-plugin-record-modifier", "0.6.2"
 else
-  download "fluent-plugin-record-modifier", "2.0.1"
+  download "fluent-plugin-record-modifier", "2.1.0"
   unless windows?
     download "systemd-journal", "1.3.3"
     download "fluent-plugin-systemd", "1.0.2"
@@ -64,5 +64,5 @@ download "fluent-plugin-td-monitoring", "0.2.4"
 if windows?
   download 'win32-eventlog', '0.6.7'
   download 'winevt_c', '0.6.1'
-  download 'fluent-plugin-windows-eventlog', '0.4.3'
+  download 'fluent-plugin-windows-eventlog', '0.4.4'
 end
