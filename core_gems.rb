67110736a302f2038a117d57788d26e302537e55
Masahiro Nakagawa <repeatedly@gmail.com>
2020-01-24 17:36:07 +0900

    Update core gems and add ext_monitor gem

--- core_gems.rb
+++ core_gems.rb
@@ -3,22 +3,18 @@ download "bundler", "1.16.6"
 download "msgpack", "1.3.1"
 if td_agent_2?
   download "cool.io", "1.5.1"
+  download "oj", "2.18.5"
 else
   download "cool.io", "1.5.4"
+  download 'serverengine', '2.2.1'
+  download "oj", "3.8.1"
+  download 'ext_monitor', '0.1.1'
 end
 download "http_parser.rb", "0.6.0"
 download "yajl-ruby", "1.4.1"
 download "sigdump", "0.2.4"
-if td_agent_2?
-  download "oj", "2.18.5"
-else
-  download "oj", "3.7.12"
-end
 download "tzinfo", "2.0.1"
 download "tzinfo-data", "1.2019.3"
-unless td_agent_2?
-  download 'serverengine', '2.2.1'
-end
 if windows?
   download 'ffi', '1.12.1'
   download 'ffi-win32-extensions', '1.0.3'
