7679c9d0ed262fa1423711ce5a6133f01c827b37
Masahiro Nakagawa <repeatedly@gmail.com>
2020-04-23 20:32:43 +0900

    update fluentd to include more patches

--- config/projects/td-agent3.rb
+++ config/projects/td-agent3.rb
@@ -24,7 +24,7 @@ override :zlib, :version => '1.2.11'
 override :jemalloc, :version => '4.5.0'
 override :rubygems, :version => '2.6.14'
 override :postgresql, :version => '9.6.9'
-override :fluentd, :version => 'eaf68654d4d80071a4a149daab5579f094f8ce37' # v1.10.2 with windows patch
+override :fluentd, :version => 'fddde2ff1f5b1cf8d0d27cee3f59f289b00b257d' # v1.10.2 with windows patch
 
 # td-agent dependencies/components
 dependency "td-agent"
