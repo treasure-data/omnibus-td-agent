0cc1fc9cb5092a566a6050d112d4c9c0f85e1762
Masahiro Nakagawa <repeatedly@gmail.com>
2020-04-24 03:28:54 +0900

    fix async gem version to avoid 2.4 drop issue

--- core_gems.rb
+++ core_gems.rb
@@ -8,7 +8,10 @@ else
   download "cool.io", "1.6.0"
   download 'serverengine', '2.2.1'
   download "oj", "3.8.1"
-  download "async-http", "0.50.7"
+  download "async", "1.24.2"
+  download "async-io", "1.27.7"
+  download "async-pool", "0.2.0"
+  download "async-http", "0.50.8"
 end
 download "http_parser.rb", "0.6.0"
 download "yajl-ruby", "1.4.1"
