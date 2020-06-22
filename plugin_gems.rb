6ad047a0cc9eb92562a7c08c1dfb65f89a7cede9
Masahiro Nakagawa <repeatedly@gmail.com>
2020-06-23 07:17:18 +0900

    update plugin gems

--- plugin_gems.rb
+++ plugin_gems.rb
@@ -6,7 +6,7 @@ if td_agent_2?
   download "fluent-plugin-td", "0.10.29"
 else
   download "td-client", "1.0.7"
-  download "td", "0.16.8"
+  download "td", "0.16.9"
   download "fluent-plugin-td", "1.1.0"
 end
 if td_agent_2?
@@ -14,13 +14,13 @@ if td_agent_2?
   download "fluent-plugin-s3", "0.8.7"
 else
   download "jmespath", "1.4.0"
-  download "aws-partitions", "1.297.0"
-  download "aws-sigv4", "1.1.1"
-  download "aws-sdk-core", "3.94.0"
-  download "aws-sdk-kms", "1.30.0"
-  download "aws-sdk-sqs", "1.24.0"
-  download "aws-sdk-s3", "1.63.0"
-  download "fluent-plugin-s3", "1.3.1"
+  download "aws-partitions", "1.332.0"
+  download "aws-sigv4", "1.2.0"
+  download "aws-sdk-core", "3.100.0"
+  download "aws-sdk-kms", "1.34.1"
+  download "aws-sdk-sqs", "1.28.0"
+  download "aws-sdk-s3", "1.69.1"
+  download "fluent-plugin-s3", "1.3.2"
 end
 if td_agent_2?
   download "thrift", "0.8.0"
@@ -33,23 +33,23 @@ download "webhdfs", "0.9.0"
 if td_agent_2?
   download "fluent-plugin-webhdfs", "0.7.1"
 else
-  download "fluent-plugin-webhdfs", "1.2.4"
+  download "fluent-plugin-webhdfs", "1.2.5"
 end
 if td_agent_2?
   download "fluent-plugin-rewrite-tag-filter", "1.6.0"
 else
-  download "fluent-plugin-rewrite-tag-filter", "2.2.0"
+  download "fluent-plugin-rewrite-tag-filter", "2.3.0"
 end
-download "ruby-kafka", "1.0.0"
+download "ruby-kafka", "1.1.0"
 unless windows?
-  download "rdkafka", "0.7.0"
+  download "rdkafka", "0.8.0"
 end
 download "fluent-plugin-kafka", "0.13.0"
 unless td_agent_2?
-  download "elasticsearch", "6.8.1"
-  download "fluent-plugin-elasticsearch", "4.0.7"
+  download "elasticsearch", "6.8.2"
+  download "fluent-plugin-elasticsearch", "4.0.9"
   download "prometheus-client", "0.9.0"
-  download "fluent-plugin-prometheus", "1.7.3"
+  download "fluent-plugin-prometheus", "1.8.0"
   download "fluent-plugin-prometheus_pushgateway", "0.0.2"
 end
 if td_agent_2?
@@ -64,6 +64,6 @@ end
 download "fluent-plugin-td-monitoring", "0.2.4"
 if windows?
   download 'win32-eventlog', '0.6.7'
-  download 'winevt_c', '0.7.3'
-  download 'fluent-plugin-windows-eventlog', '0.5.4'
+  download 'winevt_c', '0.8.1'
+  download 'fluent-plugin-windows-eventlog', '0.7.0'
 end
