c3ca606b19e531dcd5cd4f7cbf9fc42a19929f11
sophieyfang <sophieyfang@google.com>
2021-02-26 16:33:10 -0500

    Pin ffi use 1.14.1 as 1.14.2 contains LoadError bug (#317)

--- windows-installer/generate_sdl_agent_exe.ps1
+++ windows-installer/generate_sdl_agent_exe.ps1
@@ -145,6 +145,8 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 $gem_installer = $SRC_ROOT + '\bin\gem_installer'
 $core_gems_rb = $SRC_ROOT + '\core_gems.rb'
 $plugin_gems_rb = $SRC_ROOT + '\plugin_gems.rb'
+# Pin ffi version until https://github.com/ffi/ffi/issues/868 is resolved.
+& $GEM_CMD install ffi:1.14.1 --no-document
 & $GEM_CMD install fluentd:1.11.2 --no-document
 & $RUBY_EXE $gem_installer $core_gems_rb
 & $RUBY_EXE $gem_installer $plugin_gems_rb
